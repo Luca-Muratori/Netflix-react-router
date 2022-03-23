4c352bfc586ed14570cfb1e51d53bda3efe987c6
Lucai994 <luca.muratori.94@hotmail.it>
2022-03-23 15:34:15 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import "./components/styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import MovieDetails from "./components/MovieDetails";
 
-import MyFooter from "./components/MyFooter";
-import MyHeader from "./components/MyHeader";
-
-import Gallery from "./components/Gallery";
-import MyNavbar from "./components/Navbar";
-
-function App() {
-  return (
-    <>
-      <MyNavbar
-        logoWidth={"100%"}
-        iconWidth={"40px"}
-        bgColor={"black"}
-        textColor={"white"}
-        fontSize={"20px"}
-        logoSmartphoneWidth={"180px"}
-      />
-      <MyHeader />
-      <Gallery />
-      <MyFooter />
-    </>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/details/:movieID" element={<MovieDetails />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,73 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+
+const MovieDetails = () => {
+  const [details, setDetails] = useState(null);
+  const [comments, setComments] = useState([]);
+
+  const params = useParams();
+
+  useEffect(() => {
+    const fetchDetails = async () => {
+      try {
+        let resp = await fetch(
+          "http://www.omdbapi.com/?apikey=a0d093ea&i=" + params.movieID
+        );
+        if (resp.ok) {
+          let data = await resp.json();
+          console.log(data);
+          setDetails(data);
+        } else {
+          console.log("error fetching details");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const fetchComments = async () => {
+      try {
+        let response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" +
+            params.movieID,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjA1MGFjY2RhNDBjOTAwMTVmYzhkNjkiLCJpYXQiOjE2NDgwNDU3MTgsImV4cCI6MTY0OTI1NTMxOH0.-tYVF7yz73cX-Gdcvxpqtgp87xlOFTCDOJnSu2DHx7k",
+            },
+          }
+        );
+        if (response.ok) {
+          let data = await response.json();
+          setComments(data);
+        } else {
+          console.log("error fetching comments");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDetails();
+    fetchComments();
+  }, [params.movieID]);
+
+  return (
+    <div className="text-center text-white">
+      {details && (
+        <>
+          <h2>{details.Title}</h2>
+          <img src={details.Poster} alt="movie poster" />
+          <ul style={{ listStyleType: "none" }}>
+            {comments.map((c) => (
+              <li className="my-3" key={c._id}>
+                {c.comment}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default MovieDetails;
